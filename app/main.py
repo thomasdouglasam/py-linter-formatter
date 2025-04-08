fb81755bdb7887a07042891ce643af0469d69ed3
thomasdouglasam <thomasdouglasam@gmail.com>
2025-04-08 11:25:00 -0300

    Reviewed

--- app/main.py
+++ app/main.py
@@ -8,11 +8,7 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{"line": error["line_number"],
-                        "column": error["column_number"],
-                        "message": error["text"],
-                        "name": error["code"],
-                        "source": "flake8", }
+    return {"errors": [format_linter_error(error)
                        for error in errors],
             "path": file_path,
             "status": "failed" if errors else "passed"}
@@ -22,13 +18,8 @@ def format_linter_report(linter_report: dict) -> list:
     return [
         {
             "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                } for error in errors
+                format_linter_error(error)
+                for error in errors
             ],
             "path": file_path,
             "status": "failed" if errors else "passed"
