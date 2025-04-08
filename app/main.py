65037f67c7848d737bb887adb2ac5580427b778f
thomasdouglasam <thomasdouglasam@gmail.com>
2025-04-08 11:57:18 -0300

    Reviewed #1

--- app/main.py
+++ app/main.py
@@ -16,13 +16,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [
-                format_linter_error(error)
-                for error in errors
-            ],
-            "path": file_path,
-            "status": "failed" if errors else "passed"
-        }
-        for file_path, errors in linter_report.items()
-    ]
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()]
